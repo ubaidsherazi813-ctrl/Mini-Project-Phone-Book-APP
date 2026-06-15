737192ecbf114f11717cfddad6bdba685b63a60a
ubaid sherazi <235460068+ubaidsherazi813-ctrl@users.noreply.github.com>
2026-06-15 17:33:47 +0500

    Add files via upload

--- Code28.py
+++ Code28.py
@@ -1,50 +1,75 @@
+# Creating a Phone Book app Using Dictionary
+
 phone_book = {}
 
 while True:
-    print("\n--- Phone Book ---")
-    print("1. Add Contact")
-    print("2. Lookup Contact")
-    print("3. Delete Contact")
-    print("4. View All Contacts")
-    print("5. Exit")
+
+    print("\n---Phone book app---")
+    print("Enter: 1 for add contact")
+    print("Enter: 2 for lookup contact")
+    print("Enter: 3 for delete contact")
+    print("Enter: 4 for update contact")
+    print("Enter: 5 for view all contact")
+    print("Enter: 6 for Quit")
 
     choice = input("Enter your choice: ")
 
-    if choice == "1":
-        name = input("Enter name: ")
-        number = input("Enter phone number: ")
+    if choice == '1':
+
+        name = input("Enter your name: ")
+        number = int(input("Enter your phone number: "))
 
         phone_book[name] = number
-        print("Contact added successfully!")
+        print("\nContact Successfully added!\n")
 
-    elif choice == "2":
-        name = input("Enter name to search: ")
+    elif choice == '2':
+
+        name = input("Enter your name: ")
 
         if name in phone_book:
-            print(f"{name}: {phone_book[name]}")
-        else:
-            print("Contact not found!")
+            print(f"{name}: {phone_book[name]}\n")
 
-    elif choice == "3":
-        name = input("Enter name to delete: ")
+        else:
+            print("\ncontact not found!\n")
+
+    elif choice == '3':
+        name  = input("Enter your name to delete: ")
 
         if name in phone_book:
             del phone_book[name]
-            print("Contact deleted!")
-        else:
-            print("Contact not found!")
+            print("\nContact deleted\n")
 
-    elif choice == "4":
+        else:
+            print("\ncontact not found!\n")
+        
+    elif choice == '4':
+        name = input("Enter contact name: ")
+
+        if name in phone_book:
+            new_number = input("Enter new number: ")
+            phone_book[name] = new_number
+            print("\nContact updated!\n")
+        else:
+            print("\nContact not found!\n")
+
+    elif choice == '5':
         if len(phone_book) == 0:
-            print("Phone book is empty.")
+            print("\nPhone book is empty!\n")
         else:
-            print("\nContacts:")
-            for name, number in phone_book.items():
-                print(name, ":", number)
+            print("--- All COntacts ---")
+            for name , number , in phone_book.items():
+                print(f"\n{name} : {number}\n")
 
-    elif choice == "5":
-        print("Goodbye!")
+    elif choice == '6':
+        print("GoodBye!")
         break
 
     else:
-        print("Invalid choice!")
+        print("Invalid number!")   
+                        
+
+
+
+
+
+
